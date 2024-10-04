8bdbe5858bb96577a8b4e9b957a9d837156f1503
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-10-04 18:21:20 +0100

    feat: complete placeShip method and draft: receiveAttack method for the gameboard

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -1,3 +1,5 @@
+import Ship from "./ship";
+
 export default class GameBoard {
   constructor() {
     this.grid = this.createGrid();
@@ -16,7 +18,58 @@ export default class GameBoard {
 
     return grid;
   }
-}
 
-// Gameboards should be able to place ships at specific coordinates
-// by calling the ship class.
+  placeShip(row, col, shipLength, direction){
+    const ship = new Ship(shipLength);
+
+    if (direction === 'horizontal') {
+      // Check if ship fits in the grid, horizontally.
+      if (col + shipLength > this.grid[row].length) {
+        throw new Error('Ship does not fit horizontally');
+      }
+
+      // Check if space available
+      for (let i = 0; i < shipLength; i++) {
+        if (this.grid[row][col + i] !== null) {
+          throw new Error('Space already occupied');
+        }
+      }
+
+      // Place the ship
+      for (let i = 0; i < shipLength; i++) {
+        this.grid[row][col + i] = ship;
+      }
+    }
+
+    else if (direction === 'vertical') {
+      // Check if ship fits in the grid, vertically.
+      if (row + shipLength > this.grid.length) {
+        throw new Error('Ship does not fit vertically');
+      }
+
+      // Check if space available
+      for (let i = 0; i < shipLength; i++) {
+        if (this.grid[row + i][col] !== null) {
+          throw new Error('Space already occupied');
+        }
+      }
+
+      // Place the ship
+      for (let i = 0; i < shipLength; i++) {
+        this.grid[row + i][col] = ship;
+      }
+    } 
+  }
+
+  receiveAttack(row, col) {
+    if (this.grid[row][col] !== null) {
+      const ship = this.grid[row][col];
+      ship.hit();
+      if (ship.isSunk()) {
+        return true; // confirmed sunk
+      }
+      return true; // confirmed hit
+    }
+    return false; // confirmed miss
+  }
+}
