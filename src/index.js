67d2078a7a2f97cd90ad797e56325d0eb1f4357b
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-10-07 18:12:17 +0100

    refactor: move handleCellClick to game class

--- src/index.js
+++ src/index.js
@@ -20,19 +20,4 @@ const playerBoardElement = document.querySelector('#player-board');
 const computerBoardElement = document.querySelector('#computer-board');
 
 renderBoard(playerBoardElement, game.player.board, true);
-renderBoard(computerBoardElement, game.computer.board, false, handleCellClick);
-
-function handleCellClick(row, col) {
-  if (game.currentTurn === game.player) {
-    game.playerAttack(row, col);
-    game.checkGameOver();
-    console.log("Player attacked:", row, col);
-  }
-  
-  if (game.currentTurn === game.computer) {
-    game.computerAttack();
-    game.checkGameOver();
-    console.log("Computer attacked");
-  }
-}
-
+renderBoard(computerBoardElement, game.computer.board, false, game.handleCellClick.bind(game));
--- src/modules/game.js
+++ src/modules/game.js
@@ -1,29 +1,17 @@
+import { renderAttack } from "./dom";
 import Player from "./player";
 
 export default class Game {
   constructor() {
     this.player = new Player('player');
     this.computer = new Player('computer');
-    this.currentTurn = this.player;
   }
 
-  switchTurn() {
-    this.currentTurn = this.currentTurn === this.player ? this.computer : this.player;
-  }
-
-  // revisit player and computer attacks, something isn't making sense to me right now
-  // in terms of logging hits and misses and attack grid placements, blahhhhhh!!
   playerAttack(row, col) {
-    if (this.currentTurn === this.player) {
-      const result = this.computer.board.receiveAttack(row, col);
-      if (result) {
-        this.switchTurn();
-      }
-    }
+      this.computer.board.receiveAttack(row, col);
   }
 
   computerAttack() {
-    if (this.currentTurn === this.computer) {
       let row, col;
 
       do {
@@ -31,22 +19,29 @@ export default class Game {
         col = Math.floor(Math.random() * 10);
       } while (this.player.board.missedAttacks.has(`${row}, ${col}`) || this.player.board.hitAttacks.has(`${row}, ${col}`));
 
-      const result = this.player.board.receiveAttack(row, col);
-      if (result) {
-        this.switchTurn();
-      }
+      this.player.board.receiveAttack(row, col);
     }
     
-  }
-
   checkGameOver() {
     if (this.computer.board.areAllShipsSunk()) {
       alert('You win, all enemy ships are sunk!');
-      this.resetGame();
+      return true;
     } else if (this.player.board.areAllShipsSunk()) {
       alert('You lose, all your ships are sunk!');
-      this.resetGame();
+      return true;
     }
+    return false;
+  }
+
+  handleCellClick(row, col) {
+    this.playerAttack(row, col);
+    console.log("Player attacked:", row, col);
+    // renderAttack(row, col); // wip
+    if (this.checkGameOver()) return;
+    
+    this.computerAttack();
+    console.log("Computer attacked");
+    this.checkGameOver();
   }
 
   resetGame() {
