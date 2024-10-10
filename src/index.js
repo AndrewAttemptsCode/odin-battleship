8064b5d31db2843a12aa9a7df9676f5ec5a2a2f4
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-10-10 17:50:27 +0100

    refactor: add a randomzeShips method call for player and computer instead of explicit moves

--- src/index.js
+++ src/index.js
@@ -4,17 +4,8 @@ import Game from './modules/game';
 
 const game = new Game();
 
-game.player.board.placeShip(0, 0, 5, 'vertical');
-game.player.board.placeShip(0, 1, 4, 'vertical');
-game.player.board.placeShip(0, 2, 3, 'vertical');
-game.player.board.placeShip(0, 3, 3, 'vertical');
-game.player.board.placeShip(0, 4, 2, 'vertical');
-
-game.computer.board.placeShip(1, 0, 5, 'vertical');
-game.computer.board.placeShip(1, 1, 4, 'vertical');
-game.computer.board.placeShip(1, 2, 3, 'vertical');
-game.computer.board.placeShip(1, 3, 3, 'vertical');
-game.computer.board.placeShip(1, 4, 2, 'vertical');
+game.randomizeShips(game.player);
+game.randomizeShips(game.computer);
 
 const playerBoardElement = document.querySelector('#player-board');
 const computerBoardElement = document.querySelector('#computer-board');
